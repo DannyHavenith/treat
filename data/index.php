ab01a7f098711f9ee5aef8e2f9f6b9e3861c2d0a
Danny <dh.programming@havenith.net>
2012-11-28 02:24:55 +0100

    Adding license and readme.

--- data/index.php
+++ data/index.php
@@ -1,1 +1,9 @@
 <?php
+/**
+ ** Treat - Tool for reporting and analysis of test results
+ ** Copyright (c) 2012 Danny Havenith
+ ** Distributed under the Boost Software License, Version 1.0. (See
+ ** accompanying file LICENSE_1_0.txt or copy at
+ ** http://www.boost.org/LICENSE_1_0.txt)
+ **
+ */
